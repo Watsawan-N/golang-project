eedc7c86d6261ba5a77182619f029039ece46527
Watsawan <watsawan.nanthasuwannachot@krungsrinimble.com>
2025-03-08 13:23:12 +0700

    get repo

--- pkg/repository/example_repository.go
+++ pkg/repository/example_repository.go
@@ -1,10 +1,19 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"golang-project/pkg/entity"
+
+	"gorm.io/gorm"
+)
 
 type IExampleRepository interface {
 	// ExampleMethod
 	GetById(id int) (string, error)
+	Create(input entity.Example) (id uint, err error)
+	Delete(id uint) (err error)
+	TestGetById(id uint) (result *entity.Example, err error)
+	Get() (result []entity.Example, err error)
+	Update(input entity.Example) (err error)
 }
 
 type ExampleRepository struct {
@@ -19,4 +28,49 @@ func MakeIExampleRepository(db *gorm.DB) IExampleRepository {
 
 func (r *ExampleRepository) GetById(id int) (string, error) {
 	return "test repository", nil
+}
+
+func (r ExampleRepository) Create(input entity.Example) (id uint, err error) {
+	tx := r.DB.Create(&input)
+	if tx.Error != nil {
+		return 0, tx.Error
+	}
+
+	return input.ID, nil
+}
+
+func (r ExampleRepository) Update(input entity.Example) (err error) {
+	tx := r.DB.Save(&input)
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	return nil
+}
+
+func (r ExampleRepository) Delete(id uint) (err error) {
+	tx := r.DB.Delete(&entity.Example{}, id)
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	return nil
+}
+
+func (r ExampleRepository) TestGetById(id uint) (result *entity.Example, err error) {
+	tx := r.DB.First(&result, id)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	return result, nil
+}
+
+func (r ExampleRepository) Get() (result []entity.Example, err error) {
+	tx := r.DB.Find(&result)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	return result, nil
 }
